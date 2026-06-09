2510f021fc839d3303751b06a814d73a66c64851
Letícia <leticia.ssantos@sptech.school>
2026-06-09 11:04:36 -0300

    Feature/20260601/endpoints dashboard (#37)
    * Homologation (#32)
    
    * feat: adiciona endpoints de consulta de cargos
    
    * feature: Implementando novos endpoints
    
    * fix: Corrigindo bugs na entity cargo
    
    * fix: Corrigindo bugs nas entitys
    
    * fix: Corrigindo bug na entity servico
    
    * feat: Adicionando CRUD de cargos
    
    ---------
    
    Co-authored-by: VitorioBearari <vitorio.bearari@sptech.school>
    Co-authored-by: felipeferronogueira <felipe.nogueira@sptech.school>
    
    * feature: crud especialidades (#33) (#34)
    
    * Homologation (#32)
    
    * feat: adiciona endpoints de consulta de cargos
    
    * feature: Implementando novos endpoints
    
    * fix: Corrigindo bugs na entity cargo
    
    * fix: Corrigindo bugs nas entitys
    
    * fix: Corrigindo bug na entity servico
    
    * feat: Adicionando CRUD de cargos
    
    ---------
    
    
    
    
    * feature: crud especialidades
    
    * ajustando o swagger para criação do usuário
    
    ---------
    
    Co-authored-by: VitorioBearari <vitorio.bearari@sptech.school>
    Co-authored-by: felipeferronogueira <felipe.nogueira@sptech.school>
    
    * feature: criação dos endpoints da dashboard
    
    ---------
    
    Co-authored-by: Ally Awada <ally.awada@sptech.school>
    Co-authored-by: VitorioBearari <vitorio.bearari@sptech.school>
    Co-authored-by: felipeferronogueira <felipe.nogueira@sptech.school>

--- src/main/java/sptech/school/backend/repository/AgendamentoRepository.java
+++ src/main/java/sptech/school/backend/repository/AgendamentoRepository.java
@@ -3,7 +3,11 @@ package sptech.school.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import sptech.school.backend.dto.DashboardDto.AgendamentoDiasSemanaDto;
+import sptech.school.backend.dto.DashboardDto.ServicoDadosDto;
 import sptech.school.backend.entity.Agendamento;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -39,4 +43,86 @@ public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> 
     List<Agendamento> findByStatusId(Long statusId);
 
     List<Agendamento> findByClienteId(Long clienteId);
+
+    @Query("""
+            SELECT COUNT(a.id)
+            FROM Agendamento a
+            WHERE a.dataHoraInicio >= :inicio
+              AND a.dataHoraInicio <= :fim
+        """)
+    Integer findByTotalAgendamentos(
+            @Param("inicio") LocalDateTime inicio,
+            @Param("fim") LocalDateTime fim
+    );
+
+    @Query("""
+            SELECT new sptech.school.backend.dto.DashboardDto.ServicoDadosDto(
+                ats.servico.nome,
+                COUNT(ats.id)
+            )
+            FROM AtendimentoServico ats
+            WHERE ats.agendamento.dataHoraInicio >= :inicio
+              AND ats.agendamento.dataHoraInicio <= :fim
+            GROUP BY ats.servico.nome
+    """)
+    List<ServicoDadosDto> findByQuantidadeDeCadaServico(
+            @Param("inicio") LocalDateTime inicio,
+            @Param("fim") LocalDateTime fim
+    );
+
+    @Query("""
+        SELECT COUNT(DISTINCT a.cliente.id)
+        FROM Agendamento a
+        WHERE a.cliente.usuario.ativo = true
+          AND a.dataHoraInicio >= :inicio
+          AND a.dataHoraInicio <= :fim
+    """)
+    Integer findByClienteAtivosNoPeriodo(
+            @Param("inicio") LocalDateTime inicio,
+            @Param("fim") LocalDateTime fim
+    );
+
+    @Query("""
+        SELECT COUNT(DISTINCT a.cliente.id)
+        FROM Agendamento a
+        WHERE a.cliente.usuario.ativo = true
+          AND a.dataHoraInicio < :inicio
+    """)
+    Integer findByClientesNovosPeriodo(
+            @Param("inicio") LocalDateTime inicio
+    );
+
+    @Query("""
+        SELECT new sptech.school.backend.dto.DashboardDto.AgendamentoDiasSemanaDto(
+            CASE FUNCTION('DAYOFWEEK', a.dataHoraInicio)
+                WHEN 1 THEN 'Domingo'
+                WHEN 2 THEN 'Segunda-feira'
+                WHEN 3 THEN 'Terça-feira'
+                WHEN 4 THEN 'Quarta-feira'
+                WHEN 5 THEN 'Quinta-feira'
+                WHEN 6 THEN 'Sexta-feira'
+                WHEN 7 THEN 'Sábado'
+            END,
+            COUNT(a)
+        )
+        FROM Agendamento a
+        WHERE a.dataHoraInicio BETWEEN :inicio AND :fim
+        GROUP BY FUNCTION('DAYOFWEEK', a.dataHoraInicio)
+    """)
+    List<AgendamentoDiasSemanaDto> findByTotalAtendimentoDiaSemana(
+            @Param("inicio") LocalDateTime inicio,
+            @Param("fim") LocalDateTime fim
+    );
+
+    @Query("""
+        SELECT COUNT(a)
+        FROM Agendamento a
+        WHERE a.status.id = 3
+          AND a.dataHoraInicio >= :inicio
+          AND a.dataHoraInicio <= :fim
+    """)
+    Integer findByTotalCancelamentos(
+            @Param("inicio") LocalDateTime inicio,
+            @Param("fim") LocalDateTime fim
+    );
 }
